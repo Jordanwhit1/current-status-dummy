9206401094b752355ea482449330c28f3a60509c
Jordanwhit1 <Jordanwhit1@aol.com>
2018-06-13 13:56:51 -0400

    Add files via upload

---
+++ current_status_dummy.py
@@ -0,0 +1,17 @@
+start = '''
+  Hi Mike Manos, there has been a recent outage at Exon.
+  Would you like to learn more?
+'''
+
+
+print (start)
+done = False
+
+while not done:
+    user_input = input()
+    if user_input == "yes":
+        print("The current p1's are...")
+        print("Would you like more information?")
+        if user_input == "Who is responding?" "yes":
+            print( "The current team responding is...")
+        done = True
